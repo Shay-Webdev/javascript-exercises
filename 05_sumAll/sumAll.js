d476ac95b496d07ef1b0a1127d5134a4f874cff1
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-01 00:14:37 +0530

    Finish javascript exercises of The Odin Project
    Finished all 7 javascript exercises in odin project loops and arrays
    lesson.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+
+    if (a < 0 || b < 0){
+        return 'ERROR';
+    }
+        
+    if (a < b){
+        for (let i = a; i <= b; i++){
+            sum += i;
+        }
+    }else if (a > b){
+        for (let i = b; i <=a; i++){
+            sum +=i;
+        }
+    }else{
+        sum = a + b;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let leapYearCheckA = year % 4  === 0;
+    let leapYearCheckB = year % 400 === 0;
+    let leapYearCheckC = year % 100 === 0;
+
+
+    if(leapYearCheckA && (leapYearCheckB || !leapYearCheckC)  ){
+        return true;
+    }else {return false;}
 };
 
 // Do not edit below this line
